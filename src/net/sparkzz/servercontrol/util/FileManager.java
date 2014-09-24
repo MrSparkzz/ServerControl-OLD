b35db8d0719f3e3393cc66ea44f3c476d0442a35
MrSparkzz <xmrsparkzz@gmail.com>
2014-09-24 11:35:57 -0400

    Initial commit

---
+++ src/net/sparkzz/servercontrol/util/FileManager.java
@@ -0,0 +1,10 @@
+package net.sparkzz.servercontrol.util;
+
+/**
+ * Created by Brendon on 9/24/2014.
+ *
+ * Public File Management API
+ */
+public class FileManager {
+
+}
---
+++ src/net/sparkzz/servercontrol/util/Logger.java
@@ -0,0 +1,34 @@
+package net.sparkzz.servercontrol.util;
+
+import java.util.logging.Level;
+
+/**
+ * Created by Brendon on 9/24/2014.
+ *
+ * Public Logger API
+ */
+public class Logger {
+
+	private java.util.logging.Logger logger;
+	private String name;
+
+	public Logger(String name) {
+		this.name = name;
+	}
+
+	public void log(Level level, String message) {
+		logger.log(level, "[" + this.name + "] " + message);
+	}
+
+	public void info(String message) {
+		logger.log(Level.INFO, "[" + this.name + "] " + message);
+	}
+
+	public void severe(String message) {
+		logger.log(Level.SEVERE, "[" + this.name + "] " + message);
+	}
+
+	public void warn(String message) {
+		logger.log(Level.WARNING, "[" + this.name + "] " + message);
+	}
+}
